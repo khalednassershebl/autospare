7cdd4c18b62f35ace7ccca84b9b5578b386c0228
_khaledshebl <khaledabdelnasser411@gmail.com>
2024-12-24 22:17:38 +0200

    .

---
+++ main.js
@@ -0,0 +1,42 @@
+//----------------------------------------------------------------
+// Toggle Navbar
+$("header .nav-trigger").on("click", function () {
+  $(".big-nav").toggleClass("big-nav-hiddin");
+  $(".close-overlay").addClass("open-over");
+});
+// $(".header .nav-logo .nav-search-icon").on("click", function () {
+//   $(".big-nav").toggleClass("big-nav-hiddin");
+//   $(".big-nav .big-nav-search .form-control").focus();
+//   $(".close-overlay").addClass("open-over");
+// });
+$(".li-link .li-link-icon").on("click", function () {
+  $(this)
+    .parent()
+    .parent()
+    .find(".nav-link-popup")
+    .removeClass("big-nav-hiddin");
+});
+//----------------------------------------------------------------
+
+$(".big-nav .close-big-nav").on("click", function () {
+  $(".big-nav").addClass("big-nav-hiddin");
+  $(".close-overlay").removeClass("open-over");
+});
+
+$(".close-link-popup").on("click", function () {
+  $(".nav-link-popup").addClass("big-nav-hiddin");
+});
+
+// All Popups
+$(".close-overlay").on("click", function () {
+  $(this).removeClass("open-over");
+  $(".big-nav").addClass("big-nav-hiddin");
+  $(".nav-link-popup").addClass("big-nav-hiddin");
+  $(".order-service").removeClass("open-popup");
+  $(".contact-container").removeClass("open-popup");
+});
+
+// Search Icon Toggle
+$(".search-icon svg").on("click", function () {
+  $(this).parent().toggleClass("show-search");
+});
